91839c0b8a317cd431d1570ac43eff6b235829de
Joel Castillo <jocastillo@records.nyc.gov>
2018-12-14 14:44:17 -0500

    Update ost-config.php

--- include/ost-config.php
+++ include/ost-config.php
@@ -39,11 +39,10 @@ define('ADMIN_EMAIL','demouser@opsgility.com');
 # ---------------------------------------------------
 # Mysql Login info
 define('DBTYPE','mysql');
-define('DBHOST','yourservername.mysql.database.azure.com');
+define('DBHOST','osticketsrv11882.mysql.database.azure.com');
 define('DBNAME','osticket');
-define('DBUSER','demouser@yourservername');
+define('DBUSER','demouser@osticketsrv11882');
 define('DBPASS','demo@pass123');
-
 # Table prefix
 define('TABLE_PREFIX','ost_');
 
